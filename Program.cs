9c63734c74fd4cb31d2b43a09bd992af3607c1b9
singhtushar689 <singhtushar689@gmail.com>
2022-11-08 20:12:38 +0530

    EmployeeWage

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Employeeattendance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Employees");
+            int EmployeeIsPresent = 1; 
+            Random random = new Random();   
+          int EmployeeInput = random.Next(0,2);
+            if (EmployeeInput == EmployeeIsPresent) 
+            Console.WriteLine("Employee is Present");
+            else
+            Console.WriteLine("Employee is Absent");
+        }
+    }
+}
