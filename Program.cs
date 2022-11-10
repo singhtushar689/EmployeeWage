d6c32bc152c6dc5d2b43815df44fecd16644c516
singhtushar689 <singhtushar689@gmail.com>
2022-11-11 00:25:16 +0530

    EmployeeWage Maximum Hours in a Month

--- Program.cs
+++ Program.cs
@@ -5,8 +5,9 @@
 
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 100;
+        public const int EMP_RATE_PER_HOUR = 400;
         public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HOURS = 200;
 
         static void Main(string[] args)
 
@@ -14,32 +15,51 @@
             int empHrs = 0;
             int empWage = 0;
             int totalEmpWage = 0;
+            int totalWorkingDays = 0;
 
-            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            while (empHrs <= MAX_HOURS && totalWorkingDays <= NUM_OF_WORKING_DAYS)
             {
+                totalWorkingDays++;
                 Random random = new Random();
-                int empCheck = random.Next(0, 3);
-                switch (empCheck)
+                int empcheck = random.Next(0, 3);
+                switch (empcheck)
                 {
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
                     case IS_PART_TIME:
                         empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
                         break;
                     default:
                         empHrs = 0;
                         break;
                 }
+
                 empWage = empHrs * EMP_RATE_PER_HOUR;
                 totalEmpWage += empWage;
-                Console.WriteLine("Emp Wage : " + empWage);
-
+                Console.WriteLine("Employee Wage is :" + empWage);
+                Console.WriteLine("Total Monthly Wage is :" + totalEmpWage);
             }
-            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
-
         }
     }
-}
+}    
+                
+
+
+
+
+
+
+               
+    
+                
+                
+                
+                
+                   
+                        
+                    
+    
+
 
 
