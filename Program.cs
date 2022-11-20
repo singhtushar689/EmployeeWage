c6679ee17d07d1f263690a94d7ba5ef302b6e264
singhtushar689 <singhtushar689@gmail.com>
2022-11-20 20:17:01 +0530

    UC7ClassMethod

--- Program.cs
+++ Program.cs
@@ -2,22 +2,19 @@
 {
     internal class Program
     {
-
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 400;
         public const int NUM_OF_WORKING_DAYS = 20;
         public const int MAX_HOURS = 200;
 
-        static void Main(string[] args)
-
+        public static int computeEmpWage()
         {
-            int empHrs = 0;
-            int empWage = 0;
-            int totalEmpWage = 0;
+            int empHrs ;
+            int totalEmpHrs = 0;
             int totalWorkingDays = 0;
 
-            while (empHrs <= MAX_HOURS && totalWorkingDays <= NUM_OF_WORKING_DAYS)
+            while (totalEmpHrs <= MAX_HOURS && totalWorkingDays <= NUM_OF_WORKING_DAYS)
             {
                 totalWorkingDays++;
                 Random random = new Random();
@@ -34,16 +31,20 @@
                         empHrs = 0;
                         break;
                 }
-
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage += empWage;
-                Console.WriteLine("Employee Wage is :" + empWage);
-                Console.WriteLine("Total Monthly Wage is :" + totalEmpWage);
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day:" + totalWorkingDays + "Emp Hours :" + empHrs);
             }
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("total Wage :" + totalEmpWage);
+            return totalEmpWage;
         }
-    }
+          static void Main(String[] args)
+          {
+            compute empWage();
+          }
+    }      
 }    
-                
+                                                                                                                                                                                                     
 
 
 
