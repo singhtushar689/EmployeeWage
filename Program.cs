95521bde0244e31bcde80d47cd764777936441cd
singhtushar689 <singhtushar689@gmail.com>
2022-11-22 20:37:35 +0530

    Save By Employee Company

--- Program.cs
+++ Program.cs
@@ -1,49 +1,20 @@
-﻿namespace Employee_Usercase
+﻿using Employeeattendance;
+
+namespace Employee_Usercase
 {
     internal class Program
     {
-        public const int IS_FULL_TIME = 1;
-        public const int IS_PART_TIME = 2;  
-
-        public static int computeEmpWage(string company , int empRatePerHour , int numOfWorkingDays , int maxHoursPerMonth)
+        static void Main(string[] args)
         {
-            int empHrs ;
-            int totalEmpHrs = 0;
-            int totalWorkingDays = 0;
-
-            while (totalEmpHrs <= maxHoursPerMonth&& totalWorkingDays <= numOfWorkingDays)
-            {
-                totalWorkingDays++;
-                Random random = new Random();
-                int empcheck = random.Next(0, 3);
-                switch (empcheck)
-                {
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-                totalEmpHrs += empHrs;
-                Console.WriteLine("Day:" + totalWorkingDays + "Emp Hours :" + empHrs);
-            }
-            int totalEmpWage = totalEmpHrs * empRatePerHour;
-            Console.WriteLine("total Wage :" + totalEmpWage);
-            return totalEmpWage;
+            EmpWageBuilderObject dMart = new EmpWageBuilderObject("Amazon", 20, 20, 100);
+            EmpWageBuilderObject jio = new EmpWageBuilderObject("Tata", 15, 20, 50);
+            dMart.computeEmpWage();
+            Console.WriteLine(dMart.toString());
+            jio.computeEmpWage();
+            Console.WriteLine(jio.toString());
         }
-          static void Main(String[] args)
-          {
-            computeEmpWage("Amazon", 20 , 20 ,300 );
-            computeEmpWage("Nike", 100, 25, 500 );
-          }
-    }      
-}    
-                                                                                                                                                                                                     
-
+    }
+}
 
 
 
