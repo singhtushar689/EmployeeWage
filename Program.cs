ae7410d40f1f2b52c635f8225fdfab74a829f414
singhtushar689 <singhtushar689@gmail.com>
2022-11-24 11:51:36 +0530

    Contacts Updated

--- Program.cs
+++ Program.cs
@@ -5,11 +5,13 @@ namespace Employee_Usercase
     internal class Program
     {
         static void Main(string[] args)
-        {
-            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
-            empWageBuilder.addCompanyEmpWage("Amazon", 10, 20, 100);
-            empWageBuilder.addCompanyEmpWage("Tata", 15, 30, 50);
-            empWageBuilder.computeEmpWage();
+        {          
+            EmpWageBuilderObject dMart = new EmpWageBuilderObject("Amazon", 10, 20, 100);
+            EmpWageBuilderObject Jio = new EmpWageBuilderObject("Tata", 15, 30, 50);
+            dMart.computeEmpWage();
+            Console.WriteLine(dMart.ToString());
+            Jio.computeEmpWage();
+            Console.WriteLine(Jio.ToString());
         }
     }
 }
